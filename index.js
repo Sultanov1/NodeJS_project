7bc3ec1af88f0993c2978f418de4bde1daa7955f
Oybek Sultanov <oibekzzz09@gmail.com>
2024-08-02 18:54:57 +0600

    Реалиазация второго задания с помощью библиотеки caesar для шифрования нашего текста.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const Vigenere = require('caesar-salad').Vigenere;
+const password = 'password'
 
 app.get('/:message', (req, res) => {
     return res.send(req.params.message);
 });
+
+app.get('/encode/:text', (req, res) => {
+   const encode = Vigenere.Cipher(password).crypt(req.params.text);
+   res.send(encode);
+});
+
+app.get('/decode/:text', (req, res) => {
+    const encode = Vigenere.Cipher(password).crypt(req.params.text);
+    res.send(encode);
+})
 
 app.listen(port, () => {
     console.log(`Server listening at http://127.0.0.1:${port}`);
