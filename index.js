d2eca001ec8ad7ddde3e29c6a32636b0b29f21cd
Oybek Sultanov <oibekzzz09@gmail.com>
2024-08-02 18:40:43 +0600

    Реализовали первое задание которое отдает эхо ответы на get запрос.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-    res.send('hello');
-})
+app.get('/:message', (req, res) => {
+    return res.send(req.params.message);
+});
 
 app.listen(port, () => {
     console.log(`Server listening at http://127.0.0.1:${port}`);
